abfbfe17d5c1962eb5bd516ceab317d72208bb03
Ali <dkao228@mail.ru>
2025-03-17 22:30:20 +0500

    Delete lab06 directory

--- lab06/builtin-functions/1.py
+++
@@ -1,7 +0,0 @@
-from functools import reduce
-
-def multiply_list(numbers):
-    return reduce(lambda x, y: x * y, numbers)
-
-numbers = [2, 3, 4, 5]
-print(multiply_list(numbers))  
--- lab06/builtin-functions/5.py
+++
@@ -1,8 +0,0 @@
-def all_true(t):
-    return all(t)
-
-tup = (True, True, False)
-print(all_true(tup)) 
-
-tup2 = (True, True, True)
-print(all_true(tup2))  
